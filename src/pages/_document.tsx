af438ec4ddeca3c49a5c2e696be4795d5eecff2c
joaoeduardodias <joaoeduardodias123@gmail.com>
2022-08-21 12:25:47 -0300

    feat: add chakra ui and initial config

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="Loja de brinquedos de madeira artesanal, construídos com o propósito de 
+          levar a alegria para as nossas crianças com brinquedos que possuem segurança, 
+          qualidade e durabilidade."
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => <h1>hello word</h1>
+
+export default Home
